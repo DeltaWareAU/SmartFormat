d18c92f2ea02cb0da5adff7020080b38086415ab
Bradley Clarke <bradley.clarke@outlook.com>
2024-05-16 15:40:51 +1000

    Expanded target frameworks and version bump
    The project's target frameworks have been expanded to include netstandard2.1, net7.0, and net8.0 in addition to the existing ones. The version has also been bumped up from 6.0.1.1 to 8.0.0.1.
    
    Two new extension classes have been added: ObjectExtensions and TypeExtensions, which provide methods for getting public properties of an object or type respectively.
    
    A package reference was removed from the project file, and some unnecessary whitespace was cleaned up as well.
    
    In the test project file, similar changes were made to expand the target frameworks and clean up formatting.

---
+++ src/DeltaWare.SDK.SmartFormat/Extensions/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    internal static class ObjectExtensions
+    {
+        public static Dictionary<string, object> GetPublicPropertiesAsDictionary(this object value)
+        {
+            Type objectType = value.GetType();
+
+            Dictionary<string, object> propertyValues = new Dictionary<string, object>();
+
+            foreach (PropertyInfo property in objectType.GetPublicProperties())
+            {
+                object propertyValue = property.GetValue(value);
+
+                propertyValues.Add(property.Name, propertyValue);
+            }
+
+            return propertyValues;
+        }
+    }
+}
---
+++ src/DeltaWare.SDK.SmartFormat/Extensions/TypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    internal static class TypeExtensions
+    {
+        public static PropertyInfo[] GetPublicProperties(this Type type)
+        {
+            return type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        }
+    }
+}
